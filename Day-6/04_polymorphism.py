aaa3b94f4885dcf91ce122398f7412968e061f5e
Keertana Bhaskar <keertanbhaskar@gmail.com>
2026-05-18 21:30:11 +0530

    Polymorphism in python

---
+++ Day-6/04_polymorphism.py
@@ -0,0 +1,44 @@
+# Polymorphism allows same method, function or operator to behave differently 
+# depending on object it is working with.
+
+# same method ,diff behavior
+class Dog:
+  def sound(self):
+    print("Dog barks")
+class Cat:
+  def sound(self):
+    print("Cat meows")
+
+d = Dog()
+c = Cat()
+
+d.sound()
+c.sound()
+
+# method overriding (compile-time polymorphism)
+
+class Animal:
+    def sound(self):
+        print("Animal sound")
+
+class Dog(Animal):
+    def sound(self):
+        print("Dog barks")
+
+
+d = Dog()
+d.sound()
+
+# method overriding(compile time)
+# Same method name with different arguments.
+class Calculator:
+
+    def add(self, a, b=0, c=0,*args):
+      total = a + b + c + sum(args)
+      print(total)
+
+c = Calculator()
+
+c.add(5)
+c.add(5, 10)
+c.add(5, 10, 15,2,7)
