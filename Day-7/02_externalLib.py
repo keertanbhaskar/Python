09648a1136a0e5838148af60013d05b414bdca4f
Keertana Bhaskar <keertanbhaskar@gmail.com>
2026-05-19 20:40:03 +0530

    External libraries in python

---
+++ Day-7/02_externalLib.py
@@ -0,0 +1,26 @@
+# pip,env => pip install package
+
+# inbuilt libraries
+# 1.math
+import math
+print(math.sqrt(16))    
+print(math.pow(2, 3))   
+print(math.pi)          
+
+# 2.random
+import random
+print(random.randint(1,100))
+print(random.choice(["A","B","C"]))
+
+# 3.datetime
+import datetime
+today = datetime.date.today()
+print("Today is:",today)
+now = datetime.datetime.now()
+print("Current time:",now)
+
+# 4.OS
+import os
+print(os.getcwd())
+os.mkdir("new_folder")
+os.rename("old.txt","new.txt")
