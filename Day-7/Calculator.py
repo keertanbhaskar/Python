1fa4d2b179475ef00944af68afd6d94eb0cc2503
Keertana Bhaskar <keertanbhaskar@gmail.com>
2026-05-20 21:46:49 +0530

    Implemented calculator using python

---
+++ Day-7/Calculator.py
@@ -0,0 +1,22 @@
+def calculator():
+  print("Simple calculator")
+  num1 = float(input("Enter first number: "))
+  num2 = float(input("Enter second number: "))
+  operator = input("Enter operator(+, -,*, /):")
+  if operator == '+':
+    print("Result:",num1+num2)
+  elif operator == '-':
+    print('Result:',num1-num2)
+  elif operator == '*':
+    print('Result:',num1*num2)
+  elif operator == '/':
+    if num2 != 0:
+      print('Result:',num1/num2)
+    else:
+      print("Cannot divided by zero")
+  else:
+    print("Invalid operator.")
+
+calculator()
+
+  
