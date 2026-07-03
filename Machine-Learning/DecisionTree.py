3eba7e89016eda863c64cefa916a6d6d0abef6f1
Keertana Bhaskar <keertanbhaskar@gmail.com>
2026-07-03 23:17:18 +0530

    Loan Approval prediction ,implemented using DecisionTree

---
+++ Machine-Learning/DecisionTree.py
@@ -0,0 +1,22 @@
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn import tree
+import matplotlib.pyplot as plt
+
+# ex: [age,income in ₹1k]
+X = [[25,30],[45,80],[35,40],[50,90],[28,25]]
+y = ['No','Yes','Yes','Yes','No'] #loan approved
+
+# Train/test 
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2)
+model = DecisionTreeClassifier()
+model.fit(X_train,y_train)
+
+# Predict
+print(model.predict([[40,60]]))
+
+
+# visualizing the tree
+plt.figure(figsize=(10,6))
+tree.plot_tree(model,filled=True)
+plt.show()
