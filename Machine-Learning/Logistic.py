14132d1c059bd794029c84f63875ea1006532bce
Keertana Bhaskar <keertanbhaskar@gmail.com>
2026-07-05 22:24:01 +0530

    Updated the sample data

--- Machine-Learning/Logistic.py
+++ Machine-Learning/Logistic.py
@@ -5,15 +5,40 @@ from sklearn.metrics import classification_report,precision_score, recall_score,
 
 
 # Sample data
-messages = ["Win cash now", "Your OTP is 4567", "Free vacation offer", "Let's meet tomorrow"]
-labels = [1, 0, 1, 0]  # 1 = Spam, 0 = Not spam
+messages = [
+    "Win cash now",
+    "Free vacation offer",
+    "Claim your reward",
+    "Congratulations you won",
+    "Exclusive discount",
+    "Get free money",
+    "Lottery winner",
+    "Click here to claim",
+    "Your OTP is 4567",
+    "Let's meet tomorrow",
+    "Project discussion",
+    "Call me tonight",
+    "Assignment submission",
+    "Meeting at 5 PM",
+    "Happy birthday",
+    "See you in class",
+    "Dinner tonight",
+    "Can we talk",
+    "Your interview is scheduled",
+    "Please submit report"
+]
+
+labels = [
+    1,1,1,1,1,1,1,1,
+    0,0,0,0,0,0,0,0,0,0,0,0
+]
 
 # Convert text to numerical features
 vectorizer = CountVectorizer()
 X = vectorizer.fit_transform(messages)
 
 # train-test split
-X_train, X_test, y_train, y_test = train_test_split(X,labels,test_size=0.25)
+X_train, X_test, y_train, y_test = train_test_split(X,labels,test_size=0.25,random_state=42)
 
 # train Model
 model = LogisticRegression()
