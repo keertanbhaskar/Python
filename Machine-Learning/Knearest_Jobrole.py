9bd0bde6d09caf0164f51c301e6e86a64ed2d741
Keertana Bhaskar <keertanbhaskar@gmail.com>
2026-07-07 23:25:44 +0530

    implemented Job role prediction using KNN algorithm

--- Machine-Learning/Knearest_Jobrole.py
+++ Machine-Learning/Knearest_Jobrole.py
@@ -1,27 +1,24 @@
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.preprocessing import LabelEncoder
+import pandas as pd
 
-# Dataset
-data = {
-    'Python': [9,8,2,3,8,7,4,3,5,9,2,8],
-    'Java': [3,2,9,10,2,3,3,2,4,1,8,2],
-    'WebDev': [2,3,3,2,9,10,2,1,3,4,4,8],
-    'ML': [10,9,1,1,2,2,1,2,2,8,1,3],
-    'DevOps': [1,2,2,1,3,2,10,9,8,2,1,2],
-    'DSA': [7,8,8,9,7,8,7,8,9,6,7,7],
-    'JobRole': [
-        'ML Engineer',
-        'ML Engineer',
-        'Java Developer',
-        'Java Developer',
-        'Full Stack Developer',
-        'Full Stack Developer',
-        'DevOps Engineer',
-        'DevOps Engineer',
-        'DevOps Engineer',
-        'ML Engineer',
-        'Java Developer',
-        'Full Stack Developer'
-    ]
-}
+df = pd.read_csv('job_roles.csv')
+print(df.head())
+print(df.shape)
+
+
+X = df.drop('JobRole',axis=1)
+y = df['JobRole']
+
+le = LabelEncoder()
+y = le.fit_transform(y)
+
+model = KNeighborsClassifier()
+model.fit(X,y)
+
+new_candidate = pd.DataFrame([[5,5,2,1,2,1,3]],columns=X.columns)
+prediction = model.predict(new_candidate)
+print(le.inverse_transform(prediction))
+
+
 
