e72cf80a4c26d30124e737146e6319c0d3259078
Keertana Bhaskar <keertanbhaskar@gmail.com>
2026-07-07 23:35:59 +0530

    modifird the test user's preference

--- Machine-Learning/Knearest.py
+++ Machine-Learning/Knearest.py
@@ -6,7 +6,7 @@ X  = [[5,1],[1,5],[4,2],[2,4],[3,5]]
 y = ["Action", "Romance", "Action", "Romance", "Romance"]
 
 # new user's preference
-new_user = [[4,1]]
+new_user = [[6,3]]
 model = KNeighborsClassifier(n_neighbors=3)
 model.fit(X,y)
 prediction = model.predict(new_user)
