00ce86b5e21be66fc8621a862f59571fd29d88ba
Keertana Bhaskar <keertanbhaskar@gmail.com>
2026-07-08 23:11:58 +0530

    Updated the random forest model

--- Machine-Learning/DecisionTree.py
+++ Machine-Learning/DecisionTree.py
@@ -9,7 +9,7 @@ X = [[25,30],[45,80],[35,40],[50,90],[28,25]]
 y = ['No','Yes','Yes','Yes','No'] #loan approved
 
 # Train/test 
-X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2)
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2,random_state=42)
 model = DecisionTreeClassifier()
 model.fit(X_train,y_train)
 
@@ -24,4 +24,4 @@ plt.show()
 
 rd = RandomForestClassifier()
 rd.fit(X_train,y_train)
-print(model.predict([[40,60]]))
+print(rd.predict([[40,60]]))
